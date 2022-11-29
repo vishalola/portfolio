3fc56168882ee1e2febec46267683e76f787d8dd
Vishal Ola <vishalola@Vishals-MacBook-Air.local>
2022-11-29 06:55:40 +0530

    for mobile done

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ let za=0;
 let zb=0;
 let zc=0;
 let contain=document.getElementsByClassName("parent");
+let width=window.innerWidth;
 contain[0].addEventListener("mouseenter",()=>{
     a.innerHTML="Connect";
 
@@ -56,7 +57,18 @@ contain[0].addEventListener("mouseleave",()=>{
 })
 // let back=document.querySelector(".back");
 contain[0].addEventListener("click",()=>{
-    contain[0].style.marginRight="40%";
+    
+    if(width<=600)
+    {   
+        contain[0].style.height="100px";
+        contain[0].style.width="130px";
+        contain[0].style.marginTop="30%";
+    }
+    else
+    {
+        contain[0].style.marginRight="40%";
+
+    }
     // back.style.visibility="visible";
     // back.style.transform="scale(1)";
     
@@ -79,6 +91,8 @@ let skills=document.getElementById("skills");
 let project=document.getElementById("projects");
 //Temp code for change on scroll;
 contain[0].addEventListener("mousewheel",()=>{
+    if(width>600)
+    {
     contain[0].style.marginRight="0%";
     // back.style.visibility="hidden";
     // back.style.transform="scale(0)";
@@ -88,17 +102,37 @@ contain[0].addEventListener("mousewheel",()=>{
     skills.style.visibility="hidden";
     skills.style.marginLeft="-500px";
     skills.style.transform="scale(0)";
+    project.style.visibility="hidden";
+    project.style.marginLeft="-500px";
+    project.style.transform="scale(0)";
     intro.style.opacity="0%";
     setTimeout(() => {
     
         intro.innerHTML="Let me code for you.";
         intro.style.opacity="100%";
     }, 500);
+    }
 })
 a.addEventListener("click",()=>{
     za=1;
     zb=0;
     zc=0;
+    if(width<=600)
+    {
+        contain[0].style.marginBottom="0%";
+        contain[0].style.marginLeft="0%";
+        contact.style.visibility="visible";
+        contact.style.marginTop="-200px";
+        contact.style.transform="scale(1)";
+        skills.style.visibility="hidden";
+        skills.style.marginLeft="-500px";
+        skills.style.transform="scale(0)";
+        project.style.visibility="hidden";
+        project.style.marginTop="300px";
+        project.style.transform="scale(0)";
+    }
+    else
+    {
     contact.style.visibility="visible";
     contact.style.marginLeft="400px";
     contact.style.transform="scale(1)";
@@ -108,6 +142,7 @@ a.addEventListener("click",()=>{
     project.style.visibility="hidden";
     project.style.marginLeft="-500px";
     project.style.transform="scale(0)";
+    }
     intro.style.opacity="0%";
     setTimeout(() => {
     
@@ -119,6 +154,22 @@ b.addEventListener("click",()=>{
     za=0;
     zb=1;
     zc=0;
+    if(width<=600)
+    {
+    contain[0].style.marginBottom="30%";
+    contain[0].style.marginLeft="-40%";
+    skills.style.visibility="visible";
+    skills.style.marginLeft="250px";
+    skills.style.transform="scale(1)";
+    contact.style.visibility="hidden";
+    contact.style.marginTop="300px";
+    contact.style.transform="scale(0)";
+    project.style.visibility="hidden";
+    project.style.marginTop="300px";
+    project.style.transform="scale(0)";
+    }
+    else
+    {
     skills.style.visibility="visible";
     skills.style.marginLeft="400px";
     skills.style.transform="scale(1)";
@@ -128,6 +179,7 @@ b.addEventListener("click",()=>{
     project.style.visibility="hidden";
     project.style.marginLeft="-500px";
     project.style.transform="scale(0)";
+    }
     // contact.style.height="0px";
     intro.style.opacity="0%";
     setTimeout(() => {
@@ -141,6 +193,22 @@ c.addEventListener("click",()=>{
     za=0;
     zb=0;
     zc=1;
+    if(width<=600)
+    {
+        contain[0].style.marginBottom="0%";
+        contain[0].style.marginLeft="0%";
+        project.style.marginTop="-200px";
+        project.style.visibility="visible";
+        project.style.transform="scale(1)";
+        skills.style.visibility="hidden";
+        skills.style.marginLeft="-500px";
+        skills.style.transform="scale(0)";
+        contact.style.visibility="hidden";
+        contact.style.marginTop="300px";
+        contact.style.transform="scale(0)";
+    }
+    else
+    {
     project.style.visibility="visible";
     project.style.marginLeft="400px";
     project.style.transform="scale(1)";
@@ -150,6 +218,7 @@ c.addEventListener("click",()=>{
     contact.style.visibility="hidden";
     contact.style.marginLeft="-500px";
     contact.style.transform="scale(0)";
+    }
     intro.style.opacity="0%";
     setTimeout(() => {
     
